3acb56ed8167ca1cd23c283f11b2db6f15e9eca4
eftekher-alam <eftekher@yahoo.com>
2022-04-06 16:48:38 +0600

    Ch - 1(HelloWorld) and Ch - 2(DataTypes and Variables)

---
+++ 2. DataTypes And Variables/ConstantVariable/ConstantVariable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVariable
+{
+    class Program
+    {
+        const string dateOfBirth = "08 Jun 1997";
+        static void Main(string[] args)
+        {
+            const int day = 30; //It cannot change after diclaration.
+
+            //day = 50; //It will show error because it is constant variable
+
+            //dateOfBirth = "shuvo";
+
+            string id = "20";
+            int num = int.Parse(id);
+            Console.WriteLine(num);
+
+
+            Console.WriteLine(dateOfBirth);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2. DataTypes And Variables/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**** Int data type ****/
+            sbyte num = 127;
+            byte num1 = 227;
+            int num2 = 2349;
+            long num3 = 1234345324232431243;
+           
+
+            /**** Float data type ****/
+            float num5 = 7.79F;
+            double num4 = 98709.59;
+            decimal mony = 500.60M;
+
+            /**** Character data type ****/
+            char oneLetter = 'Q';
+            string name = "Eftekher Shuvo";  // String and string is same. string is standard
+
+            /**** Boolean data type ****/
+            bool complete = false;
+
+            Console.WriteLine(oneLetter);
+
+            Console.ReadKey();
+        }
+    }
+}
